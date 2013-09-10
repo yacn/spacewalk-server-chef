6da479f30addf387949079959531600550238cd3
Yet Another Clever Name <admin@yacn.pw>
2013-09-10 15:09:09 -0400

    updates metadata with supported OSs and cookbook dependencies

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,13 @@
-name             'spacewalk-server-chef'
+name             'spacewalk-server'
 maintainer       'Yet Another Clever Name'
 maintainer_email 'admin@yacn.pw'
-license          'All rights reserved'
-description      'Installs/Configures spacewalk-server-chef'
+license          'MIT'
+description      'Installs/Configures a Spacewalk Server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+%w[redhat centos].each do |os|
+  supports os, '>= 6.0'
+end
+
+depends 'yum'
