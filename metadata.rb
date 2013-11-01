fd39113852e05355af0b6d5f2bc8cb26bab8f010
Julian C. Dunn <jdunn@opscode.com>
2013-10-31 22:41:09 -0400

    Bump devodd

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'admin@yacn.pw'
 license          'MIT'
 description      'Installs/Configures a Spacewalk Server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w[fedora redhat centos].each do |os|
   supports os
