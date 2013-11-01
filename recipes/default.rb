657b1c647d4eac26c27ca98ae3651343e2874f1b
Julian C. Dunn <jdunn@opscode.com>
2013-11-01 10:32:42 -0400

    Need to symlink Spacewalk's master control script to the init.d

--- recipes/default.rb
+++ recipes/default.rb
@@ -49,7 +49,12 @@ end
 execute 'spacewalk-setup' do
   command "spacewalk-setup --disconnected --answer-file=#{Chef::Config[:file_cache_path]}/spacewalk-answers.conf"
   action :run
-  # XXX needs an idempotency guard!!
+  # XXX needs an idempotency guard!! like not_if File.exists?('/etc/rhn/rhn.conf')
+end
+
+link '/etc/init.d/spacewalk-service' do
+  to '/usr/sbin/spacewalk-service'
+  action :create
 end
 
 service 'spacewalk-service' do
